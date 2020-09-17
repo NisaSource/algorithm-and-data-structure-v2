81e81eff46176d984f71ca3f1758b594cfa4b81b
Khaerunnisa <nisa@js.local>
2020-09-17 18:02:37 +0800

    Added task 2 and 3: cache

--- cachingOptimization.js
+++ cachingOptimization.js
@@ -12,4 +12,34 @@ const uniqSort = (arr) => {
 	return result.sort((a, b) => a - b);
 };
 
+console.log('-----TASK 1-----');
 console.log(uniqSort([12, 3, 1, 54, 2, 2, 1, 12]));
+
+// Write a function that takes an argument and multiples n times 10
+
+const times10 = (num) => {
+	return num * 10;
+};
+
+console.log('-----TASK 2-----');
+console.log(times10(9));
+
+// Use an object to cache the results of times10 function.
+
+const cache = {};
+
+const memoTimes10 = (num) => {
+	if (num in cache) {
+		return `${cache[num]} is calculated`;
+	} else {
+		let result = times10(num);
+		cache[num] = result;
+		return `${result} is cached`;
+	}
+};
+
+console.log('-----TASK 3-----');
+console.log(memoTimes10(5));
+console.log(memoTimes10(9));
+console.log(memoTimes10(12));
+console.log(memoTimes10(12));

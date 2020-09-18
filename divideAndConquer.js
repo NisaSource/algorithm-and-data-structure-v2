75d0a27c0d38a96a94b03b790b7ba74ae0b70264
Khaerunnisa <nisa@js.local>
2020-09-18 16:40:02 +0800

    Added bubbleSort

--- divideAndConquer.js
+++ divideAndConquer.js
@@ -1,5 +1,4 @@
-// Implement a linear search.
-// Given a sorted array and a number, create a func
+// Given an array and a number, create a func
 // that returns at which index of the array the number is.
 
 const linearSearch = (arr, item) => {
@@ -15,4 +14,38 @@ const linearSearch = (arr, item) => {
 };
 
 console.log('----------TASK 1----------');
-console.log(linearSearch([2, 5, 8, 9, 13], 9));
+console.log(linearSearch([2, 15, 8, 34, 9, 13], 9));
+
+// create a function that accepts an unsorted array, and return the sorted array.
+
+const swapElements = (arr, i, j) => {
+	let temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+};
+
+const bubbleSort = (arr) => {
+	let countOuter = 0;
+	let countInner = 0;
+	let countSwap = 0;
+
+	let swapped;
+	do {
+		countOuter++;
+		swapped = false;
+		for (let i = 0; i < arr.length; i++) {
+			countInner++;
+			if (arr[i] && arr[i + 1] && arr[i] > arr[i + 1]) {
+				countSwap++;
+				swapElements(arr, i, i + 1);
+				swapped = true;
+			}
+		}
+	} while (swapped);
+
+	console.log('outer:', countOuter, 'inner:', countInner, 'swap:', countSwap);
+	return arr;
+};
+
+console.log('----------TASK 2----------');
+console.log(bubbleSort([2, 15, 8, 34, 9, 13]));
